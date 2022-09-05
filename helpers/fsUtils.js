b939f92639ed5bceca31f63cab161da40a2cd162
Greg <litm47@gmail.com>
2022-09-05 15:45:59 -0400

    added writetofile and readandappended functions

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -2,13 +2,29 @@
 const fs = require('fs');
 const util = require('util')
 
+// Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile)
 
 
+// writes passed through data to passed through file
+const writeToFile = (destination, content) =>
+fs.writeFile(destination, JSON.stringify(content, null, 3), (err) => 
+err ? console.log(err) : console.info(`\nData written to ${destination}`)
+);
+
+// reads specicifed file and writes passed through data as well
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if(err){
+            console.log(err)
+        }else{
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file,parsedData)
+        }
+    })
+}
 
 
 
-
-
-
-module.exports = {readFromFile}
+module.exports = {readFromFile, writeToFile, readAndAppend}
--- routes/notes.js
+++ routes/notes.js
@@ -1,38 +1,29 @@
-const notes = require('express').Router()
-const path = require('path');
-const {v4: uuidv4} = require ('uuid')
-const { readFromFile} = require('../helpers/fsUtils')
-
+const notes = require("express").Router();
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  writeToFile,
+  readAndAppend,
+} = require("../helpers/fsUtils");
 
 // GET route for all notes
-notes.get('/', (req,res) =>{
-readFromFile('./db/db.json').then((data)=> res.json(JSON.parse(data)))
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
 
-
-notes.post('/', (req,res) => {
-
-const {title, text} = req.body
-
-
-if(req.body){
+  if (req.body) {
     const newNote = {
-        title,
-        text,
-        id: uuidv4()
+      title,
+      text,
+      id: uuidv4(),
     };
+  }
+});
 
-
-
-
-}
-
-
-})
-
-
-
-
+// notes.delete(`/${id}`)
 
 module.exports = notes;
