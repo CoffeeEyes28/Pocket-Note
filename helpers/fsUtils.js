103244a559e621dbc17c04c98b752c164740296e
Greg <litm47@gmail.com>
2022-09-05 16:10:54 -0400

    new notes now post and append to page, notes can also now be deleted id is logged in console

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -9,7 +9,7 @@ const readFromFile = util.promisify(fs.readFile)
 // writes passed through data to passed through file
 const writeToFile = (destination, content) =>
 fs.writeFile(destination, JSON.stringify(content, null, 3), (err) => 
-err ? console.log(err) : console.info(`\nData written to ${destination}`)
+err ? console.log(err) : console.info(`\nUpdated ${destination}`)
 );
 
 // reads specicifed file and writes passed through data as well
--- routes/notes.js
+++ routes/notes.js
@@ -7,11 +7,13 @@ const {
   readAndAppend,
 } = require("../helpers/fsUtils");
 
+
 // GET route for all notes
 notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+// POST route for newly saved note
 notes.post("/", (req, res) => {
   const { title, text } = req.body;
 
@@ -22,11 +24,27 @@ notes.post("/", (req, res) => {
       id: uuidv4(),
     };
     readAndAppend(newNote, './db/db.json')
+   
   }else{
     res.error('Error creating new note')
   }
 });
 
-// notes.delete(`/${id}`)
+
+
+
+notes.delete('/:id', (req,res) => {
+    const noteId = req.params.id
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const result = json.filter((note) => note.id !== noteId );
+
+        writeToFile('./db/db.json', result);
+
+        res.json(`Note ${noteId} was deleted!`)
+        console.info(`Note ${noteId} was deleted!`)
+    }) 
+});
 
 module.exports = notes;
