2ef4e63dcabce62e7dc5525e8e0e6d68237ca08f
Greg <litm47@gmail.com>
2022-09-05 12:29:25 -0400

    added base router functionality

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const res = require('express/lib/response');
 const path = require('path')
-
+const uuid = require('uuid')
+const api = require('./routes/index.js')
 
 const PORT = process.env.PORT || 3001
 
@@ -10,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use('/api', api);
 
 app.use(express.static('public'));
 
