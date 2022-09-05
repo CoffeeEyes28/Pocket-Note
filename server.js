94a8f8480305d977c30e360661c38ae32da51b8f
Greg <litm47@gmail.com>
2022-09-05 12:14:55 -0400

    added get routes for html and notes page

--- server.js
+++ server.js
@@ -14,10 +14,15 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
+// GET route for homepage
+app.get('/', (req, res)=> 
+res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
-
-
-
+// GET route for notes page
+app.get('/notes', (req,res) => 
+res.sendFile(path.join(__dirname, './public/notes.html'))
+);
 
 
 
