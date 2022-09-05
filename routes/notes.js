1fed421ea8a0fdb96f28bf47d220a9105916becf
Greg <litm47@gmail.com>
2022-09-05 14:43:25 -0400

    added readfromfile functionality

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,14 @@
 const notes = require('express').Router()
 const fs = require('fs');
+const util = require('util')
 const path = require('path');
 
+const readFromFile = util.promisify(fs.readFile)
 
-notes.get('/', (req,res) =>
-res.sendFile(path.join(__dirname, '../db/db.json'))
-);
+
+notes.get('/', (req,res) =>{
+readFromFile('./db/db.json').then((data)=> res.json(JSON.parse(data)))
+});
 
 
 
