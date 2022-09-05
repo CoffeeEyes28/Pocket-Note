fba8e0d38b6ac37ed387f56bcc9cd90ab6601fd5
Greg <litm47@gmail.com>
2022-09-05 14:16:47 -0400

    added get route for notes api

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,11 @@
 const notes = require('express').Router()
+const fs = require('fs');
+const path = require('path');
 
 
+notes.get('/', (req,res) =>
+res.sendFile(path.join(__dirname, '../db/db.json'))
+);
 
 
 
